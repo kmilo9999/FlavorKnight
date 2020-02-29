fe9f72725c02b3647840e94b9c70e77c0566aeaf
wizardofcos <mcos1231@gmail.com>
2020-02-29 14:55:14 -0500

    Added left/right animations

--- Assets/TestScripts/PlayerMovement.cs
+++ Assets/TestScripts/PlayerMovement.cs
@@ -19,9 +19,23 @@ public class PlayerMovement : MonoBehaviour
     public Sprite standingUp;
     // This is a list of sprites to use when moving
     public List<Sprite> walkingUp;
-
     // Keeping track of what direction is being used
     private bool isWalkingUp;
+
+
+    // This is the sprite to use when standing still
+    public Sprite standingRight;
+    // This is a list of sprites to use when moving
+    public List<Sprite> walkingRight;
+    // Keeping track of what direction is being used
+    private bool isWalkingRight;
+
+    // This is the sprite to use when standing still
+    public Sprite standingLeft;
+    // This is a list of sprites to use when moving
+    public List<Sprite> walkingLeft;
+    // Keeping track of what direction is being used
+    private bool isWalkingLeft;
 
     // This is the sprite to use when standing still
     public Sprite standingDown;
@@ -66,7 +80,10 @@ public class PlayerMovement : MonoBehaviour
         walkingIndex = 0;
         currentTime = 0;
         isWalkingUp = false;
+        // I want to initialize this as true, so the character starts facing downward
         isWalkingDown = true;
+        isWalkingRight = false;
+        isWalkingLeft = false;
     }
 
     // Update is called once per frame
@@ -90,6 +107,17 @@ public class PlayerMovement : MonoBehaviour
                 walkingIndex = 0;
                 isWalkingUp = false;
             }
+            if (isWalkingRight) {
+                currentTime = 0;
+                walkingIndex = 0;
+                isWalkingRight = false;
+            }
+
+            if (isWalkingLeft) {
+                currentTime = 0;
+                walkingIndex = 0;
+                isWalkingLeft = false;
+            }
 
             if (currentTime > animationSpeed)
             {
@@ -107,6 +135,19 @@ public class PlayerMovement : MonoBehaviour
                 walkingIndex = 0;
                 isWalkingDown = false;
             }
+            if (isWalkingRight)
+            {
+                currentTime = 0;
+                walkingIndex = 0;
+                isWalkingRight = false;
+            }
+
+            if (isWalkingLeft)
+            {
+                currentTime = 0;
+                walkingIndex = 0;
+                isWalkingLeft = false;
+            }
 
             if (currentTime > animationSpeed)
             {
@@ -118,6 +159,69 @@ public class PlayerMovement : MonoBehaviour
             currentTime = currentTime + Time.deltaTime;
             isWalkingUp = true;
         }
+        else if (horz < -0.0001)
+        {
+            if (isWalkingDown)
+            {
+                currentTime = 0;
+                walkingIndex = 0;
+                isWalkingDown = false;
+            }
+            if (isWalkingUp)
+            {
+                currentTime = 0;
+                walkingIndex = 0;
+                isWalkingUp = false;
+            }
+            if (isWalkingRight)
+            {
+                currentTime = 0;
+                walkingIndex = 0;
+                isWalkingRight = false;
+            }
+
+            if (currentTime > animationSpeed)
+            {
+                walkingIndex++;
+                walkingIndex = walkingIndex % walkingUp.Count;
+                currentTime = currentTime - animationSpeed;
+            }
+            spriteRenderer.sprite = walkingLeft[walkingIndex];
+            currentTime = currentTime + Time.deltaTime;
+            isWalkingLeft = true;
+        }
+        else if (horz > 0.0001)
+        {
+            if (isWalkingDown)
+            {
+                currentTime = 0;
+                walkingIndex = 0;
+                isWalkingDown = false;
+            }
+            if (isWalkingUp)
+            {
+                currentTime = 0;
+                walkingIndex = 0;
+                isWalkingUp = false;
+            }
+            if (isWalkingLeft)
+            {
+                currentTime = 0;
+                walkingIndex = 0;
+                isWalkingLeft = false;
+            }
+
+            if (currentTime > animationSpeed)
+            {
+                walkingIndex++;
+                walkingIndex = walkingIndex % walkingUp.Count;
+                currentTime = currentTime - animationSpeed;
+            }
+            spriteRenderer.sprite = walkingRight[walkingIndex];
+            currentTime = currentTime + Time.deltaTime;
+            isWalkingRight = true;
+        }
+
         else
         {
             currentTime = 0;
@@ -125,6 +229,10 @@ public class PlayerMovement : MonoBehaviour
             walkingIndex = 0;
             if (isWalkingUp) {
                 spriteRenderer.sprite = standingUp;
+            } else if (isWalkingLeft){
+                spriteRenderer.sprite = standingLeft;
+            } else if (isWalkingRight) {
+                spriteRenderer.sprite = standingRight;
             } else {
                 spriteRenderer.sprite = standingDown;
             }
