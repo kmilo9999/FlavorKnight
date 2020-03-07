5f09dd698f6e13cd53d6dd7900f6fef1165de23b
Kmilo9999 <k.diaz99@gmail.com>
2020-03-07 15:46:16 -0500

    more files

---
+++ Assets/AI/AIPlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPlatform : MonoBehaviour
+{
+
+    public List<Vector2> positions;
+    // Start is called before the first frame update
+
+    Vector2 currentPosition;
+    int currentPoint = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
