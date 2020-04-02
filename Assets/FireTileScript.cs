a8177059608808be66b3fa70eef522be9dca2cd4
Kmilo9999 <k.diaz99@gmail.com>
2020-04-01 22:25:50 -0400

    added fire and switch items and animations.

---
+++ Assets/FireTileScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTileScript : MonoBehaviour
+{
+
+    public Animator animator;
+    public bool isOFF ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("isOFF", isOFF);
+    }
+}
