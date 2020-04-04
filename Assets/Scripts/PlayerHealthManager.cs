a16a7169a7092a51bbcc4ce36949fc2d0d44b347
JMigneault <jacrovm@gmail.com>
2020-04-04 15:26:29 -0400

    Add to player health script.

--- Assets/Scripts/PlayerHealthManager.cs
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class PlayerHealthManager : MonoBehaviour
 {
-    
+    public int health = 3;
+
     // called when the player dies. should reset the level once the level system is implemented.
     public void KillPlayer() {
         Camera.main.GetComponent<CameraFollow>().enabled = false;
         this.gameObject.SetActive(false);
     }
+
+    public void DealDamage(int damage) {
+        this.health -= damage;
+        if (this.health <= 0) {
+            KillPlayer();
+        }
+    }
+
+
 
 }
