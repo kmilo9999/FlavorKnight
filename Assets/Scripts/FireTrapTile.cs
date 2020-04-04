32afa005b6b03c8c8af35cdfcfc30991489614ce
JMigneault <jacrovm@gmail.com>
2020-04-04 15:02:45 -0400

    Add functionality where firetiles kill the player.

---
+++ Assets/Scripts/FireTrapTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTrapTile : MonoBehaviour
+{
+
+    public PlayerHealthManager playerHealth;
+
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.tag == "Player") {
+            Debug.Log("killing the player");
+            playerHealth.KillPlayer();
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerHealthManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+    
+    // called when the player dies. should reset the level once the level system is implemented.
+    public void KillPlayer() {
+        Camera.main.GetComponent<CameraFollow>().enabled = false;
+        this.gameObject.SetActive(false);
+    }
+
+}
