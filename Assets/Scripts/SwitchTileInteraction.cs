b26c3ad692630aee1b716ec74becfaf991ac916f
Kmilo9999 <k.diaz99@gmail.com>
2020-04-04 16:20:38 -0400

    Swtich / Fire interaction completed

---
+++ Assets/Scripts/SwitchTileInteraction.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTileInteraction : MonoBehaviour
+{
+    private bool closeToSwitch;
+    // Start is called before the first frame update
+    private GameObject switchManager;
+    void Start()
+    {
+        closeToSwitch = false;
+        switchManager = GameObject.Find("SwitchManager");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("space") && closeToSwitch)
+        {
+            switchManager.GetComponent<SwitchManager>().state = !switchManager.GetComponent<SwitchManager>().state;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            closeToSwitch = true;
+        }
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            closeToSwitch = false;
+        }
+    }
+}
